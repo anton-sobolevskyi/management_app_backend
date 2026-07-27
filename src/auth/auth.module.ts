f820056f5a064f5c047ad6eee85f0c3169da7374
Anton Sobolevskyi <anton.sobolevskyi.w@gmail.com>
2026-07-27 20:56:51 +0300

    fix auth import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,14 @@ import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [PrismaModule, PassportModule, JwtModule.register({})],
-  providers: [AuthService],
+  providers: [AuthService, JwtAuthGuard, LocalAuthGuard, JwtStrategy, LocalStrategy],
   controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule { }
