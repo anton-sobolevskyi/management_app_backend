a8a9313e97ce02b561c9ecabb4aedbef99adc977
Anton Sobolevskyi <anton.sobolevskyi.w@gmail.com>
2026-08-18 15:07:34 +0300

    fix: fix variable name

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -16,8 +16,8 @@ export class S3Service {
   constructor() {
     this.client = new S3Client({
       region: process.env.AWS_REGION,
-      endpoint: process.env.S3_ENDPOINT,
-      forcePathStyle: true, 
+      endpoint: process.env.AWS_S3_ENDPOINT,
+      forcePathStyle: true,
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
